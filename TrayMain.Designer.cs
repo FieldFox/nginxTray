ac46f2e9a3172b04b0f6b3af1df88980105c615f
wim <wim@fieldfox.be>
2014-12-16 13:24:12 +0100

    Initial Commit

---
+++ TrayMain.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace NginxTray
+{
+    partial class TrayMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrayMain));
+            this.nginxTrayItem = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toggleNginxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.versionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.htmlWwwToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.configToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnSelectExeDir = new System.Windows.Forms.Button();
+            this.tbExeDir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nginxTrayItem
+            // 
+            this.nginxTrayItem.ContextMenuStrip = this.contextMenuStrip1;
+            this.nginxTrayItem.Icon = ((System.Drawing.Icon)(resources.GetObject("nginxTrayItem.Icon")));
+            this.nginxTrayItem.Text = "Nginx";
+            this.nginxTrayItem.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toggleNginxToolStripMenuItem,
+            this.reloadToolStripMenuItem,
+            this.versionToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.htmlWwwToolStripMenuItem,
+            this.configToolStripMenuItem,
+            this.logsToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.settingsToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(140, 198);
+            // 
+            // toggleNginxToolStripMenuItem
+            // 
+            this.toggleNginxToolStripMenuItem.Name = "toggleNginxToolStripMenuItem";
+            this.toggleNginxToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.toggleNginxToolStripMenuItem.Text = "Start";
+            this.toggleNginxToolStripMenuItem.Click += new System.EventHandler(this.toggleNginxToolStripMenuItem_Click);
+            // 
+            // reloadToolStripMenuItem
+            // 
+            this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
+            this.reloadToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.reloadToolStripMenuItem.Text = "Reload";
+            this.reloadToolStripMenuItem.Click += new System.EventHandler(this.reloadToolStripMenuItem_Click);
+            // 
+            // versionToolStripMenuItem
+            // 
+            this.versionToolStripMenuItem.Enabled = false;
+            this.versionToolStripMenuItem.Name = "versionToolStripMenuItem";
+            this.versionToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.versionToolStripMenuItem.Text = "--";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(136, 6);
+            // 
+            // htmlWwwToolStripMenuItem
+            // 
+            this.htmlWwwToolStripMenuItem.Name = "htmlWwwToolStripMenuItem";
+            this.htmlWwwToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.htmlWwwToolStripMenuItem.Text = "Html / www";
+            this.htmlWwwToolStripMenuItem.Click += new System.EventHandler(this.htmlWwwToolStripMenuItem_Click);
+            // 
+            // configToolStripMenuItem
+            // 
+            this.configToolStripMenuItem.Name = "configToolStripMenuItem";
+            this.configToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.configToolStripMenuItem.Text = "Config";
+            this.configToolStripMenuItem.Click += new System.EventHandler(this.configToolStripMenuItem_Click);
+            // 
+            // logsToolStripMenuItem
+            // 
+            this.logsToolStripMenuItem.Name = "logsToolStripMenuItem";
+            this.logsToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.logsToolStripMenuItem.Text = "Logs";
+            this.logsToolStripMenuItem.Click += new System.EventHandler(this.logsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(136, 6);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(136, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // btnSelectExeDir
+            // 
+            this.btnSelectExeDir.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectExeDir.Location = new System.Drawing.Point(408, 59);
+            this.btnSelectExeDir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSelectExeDir.Name = "btnSelectExeDir";
+            this.btnSelectExeDir.Size = new System.Drawing.Size(29, 26);
+            this.btnSelectExeDir.TabIndex = 1;
+            this.btnSelectExeDir.Text = "...";
+            this.btnSelectExeDir.UseVisualStyleBackColor = true;
+            this.btnSelectExeDir.Click += new System.EventHandler(this.btnSelectExeDir_Click);
+            // 
+            // tbExeDir
+            // 
+            this.tbExeDir.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbExeDir.Location = new System.Drawing.Point(24, 59);
+            this.tbExeDir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbExeDir.Name = "tbExeDir";
+            this.tbExeDir.Size = new System.Drawing.Size(376, 29);
+            this.tbExeDir.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(417, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Select the directory where your Nginx executable is located";
+            // 
+            // btnOk
+            // 
+            this.btnOk.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnOk.Enabled = false;
+            this.btnOk.Location = new System.Drawing.Point(383, 130);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(54, 34);
+            this.btnOk.TabIndex = 4;
+            this.btnOk.Text = "Ok";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(302, 130);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 34);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click_1);
+            // 
+            // TrayMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 176);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbExeDir);
+            this.Controls.Add(this.btnSelectExeDir);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "TrayMain";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NGinx Tray";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon nginxTrayItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toggleNginxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem versionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reloadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem configToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem htmlWwwToolStripMenuItem;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnSelectExeDir;
+        private System.Windows.Forms.TextBox tbExeDir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ TrayMain.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NginxTray
+{
+    public partial class TrayMain : Form
+    {
+        private string nginxExe = @"nginx.exe";
+        
+
+        public TrayMain()
+        {
+            InitializeComponent();
+
+            nginxTrayItem.Visible = true;
+
+            initGui(isNginxRunning());
+        }
+
+        private void initGui(bool isRunning)
+        {
+            
+            versionToolStripMenuItem.Visible = false;
+
+            toggleNginxToolStripMenuItem.Text = isRunning ? "Stop" : "Start";
+            reloadToolStripMenuItem.Enabled = isRunning;
+
+            toggleNginxToolStripMenuItem.Enabled = true;
+        }
+
+
+        // NOT WORKING, could not read STDOUT from nginx
+        private string loadNginxVersion()
+        {
+            var process = getNginxProcess();
+            process.StartInfo.Arguments = "-v";
+
+            process.Start();
+
+            var version = process.StandardError.ReadToEnd();
+
+            return string.Empty; // version;
+        }
+
+        #region Process Helpers
+
+        private Process getNginxProcess()
+        {
+            string nginxWorkingDir = getNginxWorkingDir();
+            var process = new Process();
+            process.EnableRaisingEvents = true;
+            process.StartInfo.UseShellExecute = false;
+
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.FileName = Path.Combine(nginxWorkingDir, nginxExe);
+            process.StartInfo.WorkingDirectory = nginxWorkingDir;
+
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            return process;
+        }
+
+        private bool isNginxRunning()
+        {
+            var nginxProcesses = Process.GetProcessesByName("nginx");
+
+            return nginxProcesses.Count() > 0;
+        }
+
+        private string getNginxWorkingDir()
+        {
+            return NginxTray.Properties.Settings.Default.workingDir;
+        }
+
+        #endregion
+
+        #region ToolstripItems
+        private void toggleNginxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            toggleNginxToolStripMenuItem.Enabled = false;
+
+            var process = getNginxProcess();
+
+            if (isNginxRunning())
+            {
+                process.StartInfo.Arguments = "-s stop";
+                initGui(false);
+            }
+            else 
+            {
+                initGui(true);
+            }
+
+            process.Start();
+        }
+
+        private void reloadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var process = getNginxProcess();
+            process.StartInfo.Arguments = "-s reload ";
+            process.Start();
+        }
+
+        private void configToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start(Path.Combine(getNginxWorkingDir(), "conf", "nginx.conf"));
+        }
+
+        private void logsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Process.Start(Path.Combine(getNginxWorkingDir(), "log"));
+        }
+
+        private void htmlWwwToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string nginxWorkingDir = Properties.Settings.Default.workingDir;
+            Process.Start(Path.Combine(getNginxWorkingDir(), "html"));
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+
+            tbExeDir.Text = getNginxWorkingDir();
+            if (Directory.Exists(tbExeDir.Text))
+            {
+                folderBrowserDialog1.SelectedPath = tbExeDir.Text;
+            }
+
+            btnOk.Enabled = File.Exists(Path.Combine(tbExeDir.Text, nginxExe));
+
+            this.ShowDialog();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        #endregion
+
+        #region Buttons
+        private void btnSelectExeDir_Click(object sender, EventArgs e)
+        {
+            var dialogResult = folderBrowserDialog1.ShowDialog();
+
+            if (dialogResult == System.Windows.Forms.DialogResult.OK)
+            {
+                tbExeDir.Text = folderBrowserDialog1.SelectedPath;
+            }
+
+            btnOk.Enabled = File.Exists(Path.Combine(tbExeDir.Text, nginxExe));
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.workingDir = tbExeDir.Text;
+            Properties.Settings.Default.Save();
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnCancel_Click_1(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.Reset();
+            WindowState = FormWindowState.Minimized;
+        }
+        #endregion
+
+        #region Hidden Form Settings
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                this.Hide();
+            }
+        }
+
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle = myCp.ClassStyle | CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        }
+
+        #endregion
+    }
+}
